c6b24f94e5f0efea265546b59960ea87fd9fc80e
pulungbagasss <pulungbagas036@gmail.com>
2026-03-15 10:19:13 +0700

    perbarui konten surat cinta dan ubah logika password

--- mainscript.js
+++ mainscript.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // bagas ganteng banget
   submitPassword.addEventListener("click", function () {
-    if (passwordInput.value.toLowerCase() === "123") {
+    if (passwordInput.value.toLowerCase() === "bagas ganteng banget") {
       passwordModal.style.opacity = "0";
       setTimeout(() => {
         passwordModal.style.display = "none";
